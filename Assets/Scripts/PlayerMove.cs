322f6b1b4c8ba9505d1a6381ea118793eb9e5c41
Unknown <liken2525@gmail.com>
2018-01-28 10:58:33 +0900

    New: プレイヤーの魔法関係やりました。

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public enum PlayerState
+    {
+        BEFORE_PLAY,
+        PLAY,
+        CLEAR,
+        GAME_OVER
+    }
+    private PlayerState playerState = PlayerState.PLAY;
+
     private Rigidbody rb;
     /// <summary>
     /// 移動速度
@@ -17,10 +26,15 @@ public class PlayerMove : MonoBehaviour
 
     void FixedUpdate()
     {
-        // 入力を取得
-        float x_input = Input.GetAxis("Horizontal") * speed;
-        float y_input = Input.GetAxis("Vertical") * speed;
+        switch (playerState)
+        {
+            case PlayerState.PLAY:
+                // 入力を取得
+                float x_input = Input.GetAxis("Horizontal") * speed;
+                float y_input = Input.GetAxis("Vertical") * speed;
 
-        rb.velocity = new Vector3(x_input, 0, y_input);
+                rb.velocity = new Vector3(x_input, 0, y_input);
+                break;
+        }
     }
 }
